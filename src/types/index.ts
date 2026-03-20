80c988a170983c3c212fcb44d7032e0a106657f8
Vitor Bonella <51213068+VitorBonella@users.noreply.github.com>
2026-03-20 17:32:55 +0000

    feat: US-002 - Types and shared type definitions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,18 @@
-// Types will be defined in US-002
-export {}
+export interface OEmbedData {
+  title: string;
+  author_name: string;
+  thumbnail_url: string;
+}
+
+export interface StoryData {
+  title: string;
+  channelName: string;
+  thumbnailUrl: string;
+  videoId: string;
+}
+
+export interface Template {
+  id: string;
+  name: string;
+  render: (ctx: CanvasRenderingContext2D, data: StoryData) => Promise<void>;
+}
